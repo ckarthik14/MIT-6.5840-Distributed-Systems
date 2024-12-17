53d4ded89b5e9758c8947db572a75ab9c07e1d53
Karthik Cottur <karthikcottur@dhcp-10-8-018-133.mobile.reshsg.uci.edu>
2024-12-16 19:31:08 -0800

    added comments

--- src/kvsrv/server.go
+++ src/kvsrv/server.go
@@ -42,14 +42,17 @@ func (kv *KVServer) Put(args *PutAppendArgs, reply *PutAppendReply) {
 	kv.mu.Lock()
 	defer kv.mu.Unlock()
 
+	// if put has occurred in the past and retrying due to network drops, return the cached value
 	if operationResult, exists := kv.cache[args.ClientId]; exists && args.OperationNumber == operationResult.operationNumber {
 		reply.Value = operationResult.value
 		return
 	}
 
+	// put value
 	kv.store[args.Key] = args.Value
 	reply.Value = kv.store[args.Key]
 
+	// update cache with latest operation value
 	if operationResult, exists := kv.cache[args.ClientId]; exists {
 		operationResult.operationNumber = args.OperationNumber
 		operationResult.value = args.Value
@@ -62,11 +65,13 @@ func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
 	kv.mu.Lock()
 	defer kv.mu.Unlock()
 
+	// if append has occurred in the past and retrying due to network drops, return the cached value
 	if operationResult, exists := kv.cache[args.ClientId]; exists && args.OperationNumber == operationResult.operationNumber {
 		reply.Value = operationResult.value
 		return
 	}
 
+	// append value
 	oldValue, ok := kv.store[args.Key]
 	if !ok {
 		oldValue = ""
@@ -75,6 +80,7 @@ func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
 	kv.store[args.Key] = oldValue + args.Value
 	reply.Value = oldValue
 
+	// update cache with latest operation value
 	if operationResult, exists := kv.cache[args.ClientId]; exists {
 		operationResult.operationNumber = args.OperationNumber
 		operationResult.value = oldValue
